15e0f9bab0494789c27aee9619833b3a59b037cd
Morysky Yao <627571@qq.com>
2019-10-12 16:06:22 +0800

    final done with getMultiple

--- demo.php
+++ demo.php
@@ -18,6 +18,8 @@ try {
     Hbase::getInstance()->put($table, $rowKey, [['family' => 'cf', 'qualifier' => 'create_time', 'value' => '2019-04-27 18:01:12.9']]);
 
     $result = Hbase::getInstance()->get($table, $rowKey);
+
+    $result = Hbase::getInstance()->getMultiple($table, [$rowKey]);
 } catch (\Throwable $th) {
     echo 'query HBase with ' . $th->getMessage();
 }
--- src/Hbase.php
+++ src/Hbase.php
@@ -9,6 +9,7 @@ use Thrift\Protocol\TBinaryProtocol;
 use Thrift\Transport\TBufferedTransport;
 use Thrift\Transport\TSocket;
 use TPut;
+use TResult;
 
 class Hbase
 {
@@ -65,27 +66,37 @@ class Hbase
 
     public function get(string $table, string $rowKey)
     {
-        $tget      = new TGet();
-        $tget->row = static::getRealRowKeys($rowKey);
+        $tGet      = new TGet();
+        $tGet->row = static::realRowkey($rowKey);
 
-        $response = $this->client->get($table, $tget);
+        $result = $this->client->get($table, $tGet);
 
-        return array_map(function ($column) {
-            return [
-                'qualifier' => $column->qualifier,
-                'value'     => $column->value,
-                'timestamp' => $column->timestamp,
-                /* Mask useless field
-                   'family' => $column->family,
-                */
-            ];
-        }, $response->columnValues);
+        return static::toArray($result);
+    }
+
+    public function getMultiple(string $table, array $rowKeys)
+    {
+        $tGets = [];
+
+        foreach ($rowKeys as $rowKey) {
+            $tGet      = new TGet();
+            $tGet->row = static::realRowkey($rowKey);
+
+            $tGets[] = $tGet;
+        }
+
+        $results = $this->client->getMultiple($table, $tGets);
+
+        // Transfer it to plain array
+        return array_map(function ($result) {
+            return static::toArray($result);
+        }, $results);
     }
 
     public function put(string $table, string $rowKey, array $columns)
     {
-        $tput      = new TPut();
-        $tput->row = static::getRealRowKeys($rowKey);
+        $tPut      = new tPut();
+        $tPut->row = static::realRowkey($rowKey);
 
         foreach ($columns as $column) {
             $tcolumnValue = new TColumnValue();
@@ -94,14 +105,30 @@ class Hbase
             $tcolumnValue->qualifier = $column['qualifier'] ?? '';
             $tcolumnValue->value     = $column['value']     ?? '';
 
-            $tput->columnValues[] = $tcolumnValue;
+            $tPut->columnValues[] = $tcolumnValue;
         }
 
-        $this->client->put($table, $tput);
+        $this->client->put($table, $tPut);
     }
 
-    private static function getRealRowKeys(string $rowKey)
+    private static function realRowkey(string $rowKey)
     {
         return substr(md5($rowKey), 24, 8) . '_' . $rowKey;
+    }
+
+    private static function toArray($tResult)
+    {
+        if (!$tResult instanceof TResult) {
+            return [];
+        }
+
+        return array_map(function ($column) {
+            return [
+                'family'    => $column->family,
+                'qualifier' => $column->qualifier,
+                'value'     => $column->value,
+                'timestamp' => $column->timestamp,
+            ];
+        }, $tResult->columnValues);
     }
 }
