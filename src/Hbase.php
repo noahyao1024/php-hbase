fa6d17210863f5a26954c1f1861267cd1be82893
Morysky Yao <627571@qq.com>
2019-10-12 13:16:00 +0800

    Return basic array

--- src/Hbase.php
+++ src/Hbase.php
@@ -2,6 +2,7 @@
 
 namespace Hbase;
 
+use TGet;
 use THBaseServiceClient;
 use Thrift\Protocol\TBinaryProtocol;
 use Thrift\Transport\TBufferedTransport;
@@ -9,21 +10,75 @@ use Thrift\Transport\TSocket;
 
 class Hbase
 {
+    protected static $instance;
+
+    protected static $thriftHost;
+    protected static $thriftPort;
+
+    protected static $table;
+
     protected $client;
-    protected $table;
+    protected $transport;
 
-    public function __construct(string $host, int $port, string $table)
+    /**
+    * Method  getInstance
+    *
+    * @author Morysky
+    * @static
+    */
+    public static function init(string $host, int $port, string $table)
     {
-        $socket    = new TSocket($host, $port);
-        $transport = new TBufferedTransport($socket);
-        $protocol  = new TBinaryProtocol($transport);
+        static::$thriftHost = $host;
+        static::$thriftPort = $port;
 
-        $this->table  = $table;
-        $this->client = new THBaseServiceClient($protocol);
+        static::$table = $table;
+    }
+
+    /**
+     * Method  getInstance
+     *
+     * @author Morysky
+     * @static
+     * @return static
+     */
+    public static function getInstance()
+    {
+        if (static::$instance instanceof static) {
+            return static::$instance;
+        }
+
+        return static::$instance = new static;
+    }
+
+    public function __construct()
+    {
+        $socket          = new TSocket(self::$thriftHost, self::$thriftPort);
+        $this->transport = new TBufferedTransport($socket);
+        $protocol        = new TBinaryProtocol($this->transport);
+        $this->client    = new THBaseServiceClient($protocol);
+
+        $this->transport->open();
+    }
+
+    public function __destruct()
+    {
+        $this->transport->close();
     }
 
     public function get(string $rowKey)
     {
-        return $this->client;
+        $realRowKey = $this->get(static::getRealRowKeys($rowKey));
+
+        $tget      = new TGet();
+        $tget->row = $realRowKey;
+
+        $response = $this->client->get(static::$table, $tget);
+
+        return json_decode(json_encode($response->columnValues), true);
+    }
+
+    private static function getRealRowKeys(string $rowKey)
+    {
+        return substr(md5($rowKey), 24, 8) . '_' . $rowKey;
     }
 }
